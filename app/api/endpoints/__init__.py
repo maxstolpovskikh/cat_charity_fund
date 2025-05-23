5abea53d2c3638470fa12ef4151f9ea598a4ea0b
Max Stolpovskikh <maximstolpovskikh@gmail.com>
2025-05-23 23:32:51 +0700

    update

--- app/api/endpoints/__init__.py
+++ app/api/endpoints/__init__.py
@@ -1,4 +1,9 @@
-from .user import router as user_router  # noqa
-from .donation import router as donation_router  # noqa
-from .charity_project import router as charity_router  # noqa
-from .google_api import router as google_api_router  # noqa
+from .user import router as user_router
+from .donation import router as donation_router
+from .charity_project import router as charity_router
+from .google_api import router as google_api_router
+
+
+__all__ = [
+    'user_router', 'donation_router', 'charity_router', 'google_api_router'
+]
